bf4cd5e11c855768af3723c7bf77e944aa925c8f
DashAIO <102430412+DashAIO@users.noreply.github.com>
2022-08-13 23:50:36 +0200

    Update MintE_test.go

--- clients/lmnft_candy_machine/MintE_test.go
+++ clients/lmnft_candy_machine/MintE_test.go
@@ -10,19 +10,19 @@ import (
 	"testing"
 )
 
-func TestEncodeDecode_MintE(t *testing.T) {
+func TestEncodeDecode_MintEmbed(t *testing.T) {
 	fu := ag_gofuzz.New().NilChance(0)
 	for i := 0; i < 1; i++ {
-		t.Run("MintE"+strconv.Itoa(i), func(t *testing.T) {
+		t.Run("MintEmbed"+strconv.Itoa(i), func(t *testing.T) {
 			{
-				params := new(MintE)
+				params := new(MintEmbed)
 				fu.Fuzz(params)
 				params.AccountMetaSlice = nil
 				buf := new(bytes.Buffer)
 				err := encodeT(*params, buf)
 				ag_require.NoError(t, err)
 				//
-				got := new(MintE)
+				got := new(MintEmbed)
 				err = decodeT(got, buf.Bytes())
 				got.AccountMetaSlice = nil
 				ag_require.NoError(t, err)
