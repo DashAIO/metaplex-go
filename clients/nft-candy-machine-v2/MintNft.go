e1225ec39e66bc0503dadc38ca04b60585cc5166
DashAIO <102430412+DashAIO@users.noreply.github.com>
2022-09-25 21:42:00 +0200

    Update MintNft.go

--- clients/nft-candy-machine-v2/MintNft.go
+++ clients/nft-candy-machine-v2/MintNft.go
@@ -51,7 +51,7 @@ type MintNft struct {
 // NewMintNftInstructionBuilder creates a new `MintNft` instruction builder.
 func NewMintNftInstructionBuilder() *MintNft {
 	nd := &MintNft{
-		AccountMetaSlice: make(ag_solanago.AccountMetaSlice, 22),
+		AccountMetaSlice: make(ag_solanago.AccountMetaSlice, 24),
 	}
 	return nd
 }
